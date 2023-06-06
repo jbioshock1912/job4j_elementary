6355154336ac68f81ac8fa557d043b78cdb6923c
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-06-06 18:58:18 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        printMultiplicationByOne();
+    }
+
+    static void printMultiplicationByOne() {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("1 * " + i + " = " + i);
+        }
+    }
+}
