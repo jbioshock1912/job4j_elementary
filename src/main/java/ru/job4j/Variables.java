a77c8b9bb0d0fd5fdb94314e09ef3b8ba891410e
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-06-13 17:01:58 +0300

    remove comment

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Variables {
     public static void main(String[] args) {
-        int age = 18; //возраст
+        int age = 18;
         float length = 78.0332F;
         boolean baby = true;
 
