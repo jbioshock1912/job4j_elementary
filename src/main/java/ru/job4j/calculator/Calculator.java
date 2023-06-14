560710eb9370f88bb3f37cc5d3bde758bd906b31
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-06-14 10:38:12 +0300

    Арифметические операции. [#185 #422831] изменила на System.lineSeparator()

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -23,7 +23,8 @@ public class Calculator {
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo + "\n" + fiveMinusTwo + "\n" + fourTimeTwo);
+        System.out.println(sixDivTwo + System.lineSeparator() + fiveMinusTwo + System.lineSeparator()
+                + fourTimeTwo);
 
         int size = 10;
         size = 100;
