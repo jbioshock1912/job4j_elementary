67dd55218fca1f46468b39a494f97100343fc74d
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-07-24 13:06:47 +0300

    Преобразование примитивных типов [#383106]

---
+++ src/main/java/ru/job4j/calculator/CheckingTypes.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+public class CheckingTypes {
+    public static void main(String[] args) {
+        String loss = "Преобразование, которое происходит с потерей данных; ";
+        String withoutLoss = "Преобразование без потери данных; ";
+
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(loss + b);
+
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(loss + rsl);
+
+        double d = 121.19;
+        b = (byte) d;
+        System.out.println(withoutLoss + d);
+
+        short s = 1500;
+        char c = (char) s;
+        d = c;
+        System.out.println(withoutLoss + d);
+    }
+}
