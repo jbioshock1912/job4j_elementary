57017b8634ff3826fa6c0e3df739144384ba1112
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-02 10:29:15 +0300

    удалила NoWhitespaceAfter: ']' is followed by whitespace

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -5,7 +5,7 @@ public class Matrix {
         int[][] table = new int[size][size];
         for (int row = 0; row < table.length; row++) {
             for (int cell = 0; cell < table[row].length; cell++) {
-                table[row] [cell] = (cell + 1) * (row + 1);
+                table[row][cell] = (cell + 1) * (row + 1);
             }
         }
         return table;
